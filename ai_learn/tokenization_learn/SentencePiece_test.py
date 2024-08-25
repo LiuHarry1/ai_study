808043c1ad30c7740f275be0c7906b05f8162a9a
茫茫人海一粒沙 <593954214@qq.com>
2024-08-25 22:45:20 +0800

    add tf idf

--- ai_learn/tokenization_learn/SentencePiece_test.py
+++ ai_learn/tokenization_learn/SentencePiece_test.py
@@ -10,12 +10,17 @@ with open(corpus, 'w') as f:
     f.write("Tokenization is crucial in NLP.\n")
     f.write("With subword tokenization, we handle rare words effectively.\n")
 
+# https://github.com/google/sentencepiece
+# --input: one-sentence-per-line raw corpus file. No need to run tokenizer, normalizer or preprocessor. By default, SentencePiece normalizes the input with Unicode NFKC. You can pass a comma-separated list of files.
+# --model_prefix: output model name prefix. <model_name>.model and <model_name>.vocab are generated.
+# --vocab_size: vocabulary size, e.g., 8000, 16000, or 32000
+# --character_coverage: amount of characters covered by the model, good defaults are: 0.9995 for languages with rich character set like Japanese or Chinese and 1.0 for other languages with small character set.
+# --model_type: model type. Choose from unigram (default), bpe, char, or word. The input sentence must be pretokenized when using word type.
 # Train SentencePiece model with the BPE algorithm
 spm.SentencePieceTrainer.train(input=corpus, model_prefix='spm_model', vocab_size=100, model_type='bpe')
 
 # This will create 'spm_model.model' and 'spm_model.vocab'
 
-import sentencepiece as spm
 
 # Load the trained SentencePiece model
 sp = spm.SentencePieceProcessor(model_file='spm_model.model')
